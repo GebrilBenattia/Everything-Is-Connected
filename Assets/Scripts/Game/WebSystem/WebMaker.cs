2c3a3e9f311b55eeea7f14335d66ee48c0abb98e
Marc Léglise <leglisemarc71@gmail.com>
2024-09-10 12:57:13 +0200

    [Add Feature] (Code) News Segments can be linked together

--- Assets/Scripts/Game/WebSystem/WebMaker.cs
+++ Assets/Scripts/Game/WebSystem/WebMaker.cs
@@ -7,27 +7,26 @@ public class WebMaker : MonoBehaviour
     [SerializeField] private float m_RayDistance;
     [SerializeField] private string m_TagToLink;
     [SerializeField] private GameObject m_StringSegmentToSpawn;
+    [SerializeField] private float m_segmentLength;
     private Camera m_Camera;
     private System.Action m_DoAction;
     private GameObject m_SelectedNode;
     private GameObject m_StartPoint;
     private GameObject m_EndPoint;
-    private Vector3 m_Direction;
     private Vector3 m_CurrentStringStartPoint;
-    // Start is called before the first frame update
+
     void Start()
     {
-        Debug.Log("start");
         m_Camera = Camera.main;
         SetModeDefault();
     }
 
-    // Update is called once per frame
     void Update()
     {
         m_DoAction();
     }
 
+    //StateMachine
     private void SetModeDefault()
     {
         m_DoAction = DoActionDefault;
@@ -38,7 +37,7 @@ public class WebMaker : MonoBehaviour
         if (CheckForNode() && Input.GetMouseButton(0))
         {
             SetModeSowing();
-        };
+        }
     }
 
     private void SetModeSowing()
@@ -57,24 +56,20 @@ public class WebMaker : MonoBehaviour
                 Debug.Log(m_EndPoint.transform.position);
                 Debug.Log(m_EndPoint.name);
                 m_CurrentStringStartPoint = m_StartPoint.transform.position;
-                for (int i = 1; i < 4; i++)
+                int maxSegments = (int)(Vector3.Distance(m_StartPoint.transform.position, m_EndPoint.transform.position) / m_segmentLength);
+                Debug.Log(maxSegments);
+                for (int i = 1; i < maxSegments+1; i++)
                 {
                     AddSegment(i);
                 }
-
-                //m_Direction = m_EndPoint - m_StartPoint;
             }
-            else
-            {
-                SetModeDefault();
-            }
-            /*m_StartPoint = Vector3.zero;
-            m_EndPoint = Vector3.zero;*/
-            m_SelectedNode = null;
+            SetModeDefault();
+            ClearValues();
         }
 
     }
 
+    //Check for NewsObject with raycast
     private bool CheckForNode()
     {
         bool lRaycast = false;
@@ -92,21 +87,23 @@ public class WebMaker : MonoBehaviour
         return lRaycast;
     }
 
-    private void AddSegment(float pIndex)
+    //Add a web segment
+    private void AddSegment(float _Index)
     {
-        Quaternion lRotation = Quaternion.LookRotation(m_EndPoint.transform.position - m_StartPoint.transform.position);
-        GameObject segment = Instantiate(m_StringSegmentToSpawn, m_CurrentStringStartPoint, lRotation);
-        //Vector3 lStartPosDelay = segment.GetComponent<WebSegment>().m_StartPoint.localPosition;
-        //float lDistance = Mathf.Sqrt(Mathf.Pow(m_EndPoint.transform.position.x, 2) + Mathf.Pow(m_EndPoint.transform.position.z, 2));
-        float lDistance = Vector3.Distance(m_StartPoint.transform.position, m_EndPoint.transform.position);
-        float lRatio = (segment.GetComponent<WebSegment>().length / lDistance) * pIndex;
-        Debug.Log(lDistance);
-        Debug.Log(lRatio);
-        Vector3 truc = Vector3.Lerp(m_StartPoint.transform.position, m_EndPoint.transform.position, lRatio);
-        segment.transform.position = truc;
-        //Debug.Log(lStartPosDelay);
-        //segment.transform.position += new Vector3(lStartPosDelay.x, 0, lStartPosDelay.z);
-        m_CurrentStringStartPoint = segment.GetComponent<WebSegment>().m_EndPoint.position;
+        Quaternion rotation = Quaternion.LookRotation(m_EndPoint.transform.position - m_StartPoint.transform.position);
+        GameObject segment = Instantiate(m_StringSegmentToSpawn, m_CurrentStringStartPoint, rotation);
+        float distance = Vector3.Distance(m_StartPoint.transform.position, m_EndPoint.transform.position);
+        float ratio = (segment.GetComponent<WebSegment>().length / distance) * _Index;
+        Vector3 segmentPos = Vector3.Lerp(m_StartPoint.transform.position, m_EndPoint.transform.position, ratio);
+        segment.transform.position = segmentPos;
+        m_CurrentStringStartPoint = segment.GetComponent<WebSegment>().endPoint.position;
+    }
+
+    private void ClearValues()
+    {
+        m_SelectedNode = null;
+        m_StartPoint = null;
+        m_EndPoint = null;
     }
 }
 
