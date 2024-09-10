80d4f7b3015d496a4fc03353342e39f09c9235d1
l.noncent <l.noncent@student.isartdigital.com>
2024-09-10 12:01:30 +0200

    [Update Feature](Code) Update NewsSpawnManager to manage the spawn of news in different area with a spawn rate

--- Assets/Scripts/Game/NewsSystem/NewsSpawnManager.cs
+++ Assets/Scripts/Game/NewsSystem/NewsSpawnManager.cs
@@ -26,7 +26,8 @@ public class NewsSpawnManager : MonoBehaviour
 
     // Private Variables
     private float m_CurrentSpawnCooldown;
-    private int m_CurrentNewsNbr;
+    private int[] m_NewsObjectCount = new int[4];
+    private int m_TotalNewsObjectCount = 0;
 
     // ###################################### GETTER / SETTER #####################################
 
@@ -40,26 +41,93 @@ public class NewsSpawnManager : MonoBehaviour
         m_Instance = this;
         m_CurrentSpawnCooldown = Random.Range(m_MinSpawnTime, m_MaxSpawnTime);
     }
-
-    private void SpawnNews()
+    
+    private float[] CalculateSpawnRates()
     {
-        float posX = Random.Range(-m_SpawnAreaSize.x / 2f, m_SpawnAreaSize.x / 2f) + transform.position.x;
-        float posZ = Random.Range(-m_SpawnAreaSize.z / 2f, m_SpawnAreaSize.z / 2f) + transform.position.z;
-        int randNewsDataIndex = Random.Range(0, m_NewsDataList.Length);
+        // Variables
+        float[] spawnRates = new float[4];
 
+        // Calculate Spawn Rates
+        for (int i = 0; i < spawnRates.Length; ++i) {
+            spawnRates[i] = m_TotalNewsObjectCount == 0 ? 1f : 1f / (1 + m_NewsObjectCount[i]);
+        }
+
+        // Normalize Spawn Rates
+        float totalSpawnRate = 0f;
+        for (int i = 0; i < spawnRates.Length; ++i) totalSpawnRate += spawnRates[i];
+        for (int i = 0; i < spawnRates.Length; ++i) spawnRates[i] /= totalSpawnRate;
+
+        Debug.Log("RATES: (1): " + spawnRates[0].ToString() + "(2): " + spawnRates[1].ToString() + "(3): " + spawnRates[2].ToString() + "(4): " + spawnRates[3].ToString());
+
+        return spawnRates;
+    }
+
+    private int ChooseZone(float[] _SpawnRates)
+    {
+        // Variables
+        float randFloat = Random.value;
+        float cumulativeRate = 0f;
+
+        // Calculate spawn Zone 
+        for (int i = 0; i < _SpawnRates.Length; ++i) {
+
+            cumulativeRate += _SpawnRates[i];
+            if (randFloat <= cumulativeRate) return i + 1;
+        }
+
+        return 0;
+    }
+
+    private void SpawnNewsObject()
+    {
+        // Get zone Index
+        int zoneIndex = ChooseZone(CalculateSpawnRates());
+
+        Debug.Log("ZONE " + zoneIndex.ToString());
+
+        Vector2 minSpawnPoint = Vector2.zero;
+        Vector2 maxSpawnPoint = Vector2.zero;
+        switch (zoneIndex) {
+
+            case 1: 
+                minSpawnPoint = new Vector2(-m_SpawnAreaSize.x / 2f, m_SpawnAreaSize.z / 2f);
+                maxSpawnPoint = Vector2.zero;
+                break;
+
+            case 2:
+                minSpawnPoint = new Vector2(m_SpawnAreaSize.x / 2f, m_SpawnAreaSize.z / 2f);
+                maxSpawnPoint = Vector2.zero;
+                break;
+
+            case 3:
+                minSpawnPoint = new Vector2(-m_SpawnAreaSize.x / 2f, -m_SpawnAreaSize.z / 2f);
+                maxSpawnPoint = Vector2.zero;
+                break;
+
+            case 4:
+                minSpawnPoint = new Vector2(m_SpawnAreaSize.x / 2f, -m_SpawnAreaSize.z / 2f);
+                maxSpawnPoint = Vector2.zero;
+                break;
+        }
+
+        float posX = Random.Range(minSpawnPoint.x, maxSpawnPoint.x) + transform.position.x;
+        float posZ = Random.Range(minSpawnPoint.y, maxSpawnPoint.y) + transform.position.z;
+
+        int randNewsDataIndex = Random.Range(0, m_NewsDataList.Length);
         NewsObjectPoolManager.Instance.SpawnNewsObject(m_NewsDataList[randNewsDataIndex], new Vector3(posX, 0, posZ));
 
-        m_CurrentNewsNbr++;
+        m_TotalNewsObjectCount++;
+        m_NewsObjectCount[zoneIndex - 1]++;
     }
 
     private void Update()
     {
-        if (m_CurrentNewsNbr >= m_MaxNewsCount) return;
+        if (m_TotalNewsObjectCount >= m_MaxNewsCount) return;
 
         if (m_CurrentSpawnCooldown > 0) m_CurrentSpawnCooldown -= Time.deltaTime;
         else {
             m_CurrentSpawnCooldown += Random.Range(m_MinSpawnTime, m_MaxSpawnTime);
-            SpawnNews();
+            SpawnNewsObject();
         }
     }
 
