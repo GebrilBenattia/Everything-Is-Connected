82641b2428af615388dac3cf7e01dd683715ba04
Marc Léglise <leglisemarc71@gmail.com>
2024-09-12 11:42:50 +0200

    [Add Feature] (Code) Bubble sizes can be selected by code

--- Assets/Scripts/Game/UI/UIManager.cs
+++ Assets/Scripts/Game/UI/UIManager.cs
@@ -7,21 +7,29 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject m_LifeBar;
     [SerializeField] private GameObject m_TextContainer;
-    private int testCounter = 4;
+    private int testCounter = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1)) UpdateContainers(m_TextContainer, testCounter--);
+        //if (Input.GetMouseButtonDown(1)) UpdateContainers(m_TextContainer, testCounter--);
+        if (Input.GetMouseButtonDown(1)) UpdateBubbles(m_TextContainer, testCounter++);
     }
 
-    public void UpdateContainers(GameObject _Container, int _Life)
+    public void UpdateLife(GameObject _Container, int _Index)
     {
-         _Container.transform.GetChild(_Life).GetComponent<Image>().enabled = false;
+        _Container.transform.GetChild(_Index).GetComponent<Image>().enabled = false;
     }
+
+    public void UpdateBubbles(GameObject _Container, int _Index)
+    {
+        _Container.transform.GetChild(_Index).GetComponent<TextBubble>().SetNewBubble(TextBubble.TextSizes.small, "test");
+    }
+
+    //public void 
 }
